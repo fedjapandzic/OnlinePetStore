8671f45c839ffc86e17945b02afd5a01618937d5
TinRadisic <tin.radisic@stu.ibu.edu.ba>
2023-05-20 18:07:19 +0200

    Adding landing page

---
+++ frontend/petstore/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="overlay">
+        <h1>Coming soon!</h1>
+        <p>We are currently working on our webpage, it will be done in a few weeks.</p>
+        <p>We will be happy to see you again!</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
