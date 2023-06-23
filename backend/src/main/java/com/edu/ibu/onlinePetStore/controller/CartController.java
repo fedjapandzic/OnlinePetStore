fcd9173b38c3e94937eb98df86e3c01c0c87b4a2
Fedja Pandzic <fedjapandzic1@gmail.com>
2023-06-23 18:26:49 +0200

    added cart controller

--- backend/src/main/java/com/edu/ibu/onlinePetStore/controller/CartController.java
+++ backend/src/main/java/com/edu/ibu/onlinePetStore/controller/CartController.java
@@ -1,4 +1,33 @@
 package com.edu.ibu.onlinePetStore.controller;
 
+import com.edu.ibu.onlinePetStore.exception.CartNotFoundException;
+import com.edu.ibu.onlinePetStore.exception.UserNotFoundException;
+import com.edu.ibu.onlinePetStore.model.Cart;
+import com.edu.ibu.onlinePetStore.model.User;
+import com.edu.ibu.onlinePetStore.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cart")
 public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Cart cart){
+        cartService.saveCart(cart);
+        return "Cart with ID:"+ cart.getId() + " successfully added!";
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Cart> getCartById(@PathVariable("id") int id){
+        Optional<Cart> cart = cartService.getCart(id);
+        if (cart == null){
+            throw new CartNotFoundException("Cart not found with ID: " + id);
+        }
+        return cart;
+    }
 }
